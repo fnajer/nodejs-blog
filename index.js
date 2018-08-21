34f11df2fdcfec3a598caa7cb794a1c9b478dc50
fnajer <lexactr@gmail.com>
2018-08-22 00:15:16 +0300

    Create home page

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
+const path = require('path');
+const express = require('express');
 
-const app = new express()
+const app = new express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
 
 app.listen(4000, () => {
   console.log('App listening on port 4000')
-})
+});
