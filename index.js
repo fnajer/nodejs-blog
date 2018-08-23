b7b8cac4b1b808d74bc0e8b1b9f49172ed41ea7d
fnajer <lexactr@gmail.com>
2018-08-23 20:49:51 +0300

    Add views for contact and post pages

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
-})
+  res.render('contact');
+});
 
 app.get('/post', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/post.html'));
-})
+  res.render('post');
+});
 
 app.listen(4000, () => {
   console.log('App listening on port 4000')
